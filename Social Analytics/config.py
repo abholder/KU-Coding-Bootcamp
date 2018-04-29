3d292054504a14565251e03a91507a25433ed0a0
Aaron Holder <holderab@gmail.com>
2018-04-28 20:41:26 -0500

    Social Analytics Homework

---
+++ Social Analytics/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "VS9z4GUrht2f7r8TEV9r5Tims"
+consumer_secret = "68WMLKyrYAvRR0nhND8kqk8oZ4hPbr3cS5SjMUi1wQVmSuKrGH"
+access_token = "41925529-rspJRkYeuq7LsiWml4JkCSpKwM2Dp3NzeIyNOgToW"
+access_token_secret = "bhLRcgTfyRhE7M0HMO1K7aGd7B0PSck3sj8nT0sWsyg1l"
